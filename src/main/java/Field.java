0118ea1c5d6a774f73cd67f7d1db39ff8a679833
svetlana_klimova <sfettkoo@mail.ru>
2023-09-15 23:56:53 +0300

    done

---
+++ src/main/java/Field.java
@@ -0,0 +1,122 @@
+import java.util.Scanner;
+
+public class Field { //pole
+    Scanner scanner = new Scanner(System.in);
+    int countCell; // кол-во ячеек. при попадании это кол-во уменьшать на 1 //если останется 0, то игрок выиграл
+    int[][] playingField; // = new int[10][10]; //игровое поле игрока
+    int[][] opponentsField; // поле соперника
+    int countShipsPlayer; // кол - во кораблей
+
+
+    public Field(int[][] playingField, int countShipsPlayer, int[][] opponentsField, int countCell) {
+        this.playingField = playingField;
+        this.countShipsPlayer = countShipsPlayer;
+        this.countCell = countCell;
+        this.opponentsField = opponentsField;
+    }
+
+    public void drawNullField() { // нарисовать пустое поле
+        for (int x = 0; x < playingField.length; x++) {
+            for (int y = 0; y < playingField.length; y++) {
+                playingField[x][y] = 0;
+                opponentsField[x][y] = 0;
+            }
+        }
+    }
+
+    public void drawField(int[][] field) {// отображение на экране текущего поля
+        System.out.println("   0  1  2  3  4  5  6  7  8  9 ");
+        for (int x = 0; x < field.length; x++) {
+            System.out.print(x + " ");
+            for (int y = 0; y < field.length; y++) {
+                System.out.print(" " + field[x][y] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+    public void shipsOnField(String name) {
+        System.out.println(name + ", добавляйте корабль. Введите количество палуб.");
+        int deck = scanner.nextInt();
+        System.out.println("Введите координаты X");
+        int x = scanner.nextInt();
+        System.out.println("Введите координаты У");
+        int y = scanner.nextInt();
+        System.out.println("Если установить по-вертикали - введите 1. Если по горизонтали - 2");
+        int position = scanner.nextInt();
+
+        if (isCorrected(playingField, x, y, deck, position)) {
+            if (position == 1) {
+                for (int a = 0; a < deck; a++) {//  по вертикали
+                    playingField[x + a][y] = 1;
+                }
+                System.out.println("Установлен корабль по-вертикали ");
+                countShipsPlayer++;
+                countCell += deck;
+                System.out.println("кол - во кораблей: " + countShipsPlayer);
+            } else {
+                for (int a = 0; a < deck; a++) {//  по горизонтали
+                    playingField[x][y + a] = 1;
+                }
+                System.out.println("Установлен корабль по-горизонтали ");
+                countShipsPlayer++;
+                countCell += deck;
+                System.out.println("кол - во кораблей: " + countShipsPlayer);
+            }
+        } else {
+            System.out.println("Ошибка. Попробуйте ещё раз");
+            shipsOnField(name);
+        }
+    }
+
+    public void motion(int[][] field, String name, int[][] opponentsField) { // ход
+        System.out.println(name + " , ведите координаты Х");
+        int x = scanner.nextInt();
+        System.out.println("Введите координаты Y");
+        int y = scanner.nextInt();
+
+        if (field[x][y] == 1) {
+            countCell--;
+            opponentsField[x][y] = 1;
+            if (countCell != 0) {
+                System.out.println("Вы попали. Ходите еще раз.");
+                motion(field, name, opponentsField);
+            }
+            if (countCell == 0) {
+                System.out.println("Кораблей больше не осталось.");
+            }
+        }
+        if (field[x][y] == 0) {
+            System.out.println("Мимо. Переход кода другому игроку");
+            field[x][y] = 2;
+            opponentsField[x][y] = 2;
+        }
+    }
+
+    public boolean isCorrected(int[][] field, int x, int y, int deck, int position) {
+        boolean ok = false;
+        try {
+            if (position == 1 || (x + deck > field.length)) { //  по-вертикали
+                return false;
+            }
+            if (position == 2 || (y + deck > field.length)) { // по-горизонтали
+                return false;
+            }
+            if (field[x + 1][y + 1] == 1 || field[x - 1][y - 1] == 1 || field[x - 1][y + 1] == 1 || field[x + 1][y - 1] == 1) { // по диагонали
+                return false;
+            }
+            for (int i = deck; i > 0; i--) {
+                        ok = field[x][y] != 1 &&
+                        field[x - 1][y] != 1 &&
+                        field[x][y - 1] != 1 &&
+                        field[x + i][y] != 1 &&
+                        field[x][y + i] != 1;
+            }
+        }
+        catch (ArrayIndexOutOfBoundsException ex){
+            return ok;
+        }
+        return ok;
+    }
+}
---
+++ src/main/java/Main.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[][] playingField1 = new int[10][10]; //игровое поле первого игрока
+        int[][] playingField2 = new int[10][10]; //поле с кораблями второго игрока
+
+        int[][] opponentsField1 = new int[10][10]; //игровое поле соперника 1 игрока (корабли 2)
+        int[][] opponentsField2 = new int[10][10]; //игровое поле соперника 2 игрока (корабли 1)
+
+        int countShipsPlayer1 = 0; // кол - во кораблей 1го игрока
+        int countShipsPlayer2 = 0; // кол - кораблей 2го игрока
+
+        Field field1 = new Field(playingField1, countShipsPlayer1, opponentsField1, countShipsPlayer1); // поле первого игрока
+        Field field2 = new Field(playingField2, countShipsPlayer2, opponentsField2, countShipsPlayer2); //поле второго игрока
+
+        System.out.println("Игрок 1, введите ваше имя: ");
+        String namePlayer1 = scanner.next();
+        System.out.println("Игрок 2, введите ваше имя: ");
+        String namePlayer2 = scanner.next();
+
+        field1.drawNullField(); //отобразить пустое поле
+
+        while (field1.countShipsPlayer < 4 && field2.countShipsPlayer < 4) {
+            field1.shipsOnField(namePlayer1); // расстановка кораблей 1 игрока
+            field2.shipsOnField(namePlayer2); // расстановка кораблей 2 игрока
+        }
+
+        System.out.println("Игра началась.");
+
+        while (field1.countCell != 0 && field2.countCell != 0) {
+            System.out.println("Поле игрока: " + namePlayer2 + ", 0 - неизвестно, 1 - корабль, 2 - мимо");
+            field1.drawField(opponentsField1);
+            field2.motion(playingField2, namePlayer1, opponentsField1);
+            if (field2.countCell == 0) {
+                System.out.println("Поздравляем! Игрок  " + namePlayer1 + " выиграл(а)!");
+                break;
+            }
+            System.out.println("Поле первого игрока: " + namePlayer1+ ", 0 - неизвестно, 1 - корабль, 2 - мимо");
+            field2.drawField(opponentsField2);
+            field1.motion(playingField1, namePlayer2, opponentsField2);
+                if (field1.countCell == 0) {
+                    System.out.println("Поздравляем! Игрок " + namePlayer2 + " выиграл(а)!");
+                    break;
+                }
+            }
+        }
+    }
+
+
